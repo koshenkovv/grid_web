7ae5c837385b83151fa4a4cc7d66b906ee5abcb1
koshenkovv <113175964+koshenkovv@users.noreply.github.com>
2024-04-28 23:36:33 +0300

    idk what is that

---
+++ media/scripts/header_script.js
@@ -0,0 +1,9 @@
+function openProfile() {
+    document.getElementById("profile-block").style.display = "block";
+    document.getElementById("header-menu-close").style.display = "block";
+}
+
+function closeProfile() {
+    document.getElementById("profile-block").style.display = "none";
+    document.getElementById("header-menu-close").style.display = "none";
+}
